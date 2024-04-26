8bf53d6b82b871403bc86fbfce22b3c59eb89132
MuhammadRezeki1 <mrezeki745@gmail.com>
2024-04-27 01:44:04 +0700

    UTS MUHAMMAD REZEKI

--- app/src/main/java/com/example/githubuserapp/data/retrofit/ApiService.java
+++ app/src/main/java/com/example/githubuserapp/data/retrofit/ApiService.java
@@ -11,11 +11,11 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @Headers({"Authorization: token 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"})
+    @Headers({"Authorization: token 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"})
     @GET("search/users")
     Call<GithubSearchResponse> searchUsers(@Query("q") String query);
 
-    @Headers({"Authorization: token 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"})
+    @Headers({"Authorization: token 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"})
     @GET("users/{username}")
     Call<GithubUser> getUser(@Path("username") String username);
 
